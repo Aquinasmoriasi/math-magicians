da5bae9c01c2c5584f57d900666527812cc959a3
unknown <aquinasmoriasi@gmail.com>
2022-08-10 13:11:19 +0300

    add functionality to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,77 +1,95 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
-export default class Calculator extends React.PureComponent {
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    const targetBtn = e.target.value;
+    const evaluate = calculate(this.state, targetBtn);
+    this.setState(evaluate);
+  }
+
   render() {
+    const { total, next } = this.state;
     return (
       <form action="#">
-        <input type="text" value="0" />
+        <input type="text" value={total || next || 0} />
         <table>
           <tbody>
             <tr>
               <td>
-                <input type="button" value="AC" />
+                <input type="button" onClick={this.handleClick} value="AC" />
               </td>
               <td>
-                <input type="button" value="+/-" />
+                <input type="button" onClick={this.handleClick} value="+/-" />
               </td>
               <td>
-                <input type="button" value="%" />
+                <input type="button" onClick={this.handleClick} value="%" />
               </td>
               <td className="last-column">
-                <input type="button" value="&#247;" />
+                <input type="button" onClick={this.handleClick} value="&#247;" />
               </td>
             </tr>
             <tr>
               <td>
-                <input type="button" value="7" />
+                <input type="button" onClick={this.handleClick} value="7" />
               </td>
               <td>
-                <input type="button" value="8" />
+                <input type="button" onClick={this.handleClick} value="8" />
               </td>
               <td>
-                <input type="button" value="9" />
+                <input type="button" onClick={this.handleClick} value="9" />
               </td>
               <td className="last-column">
-                <input type="button" value=" &times;" />
+                <input type="button" onClick={this.handleClick} value="x" />
               </td>
             </tr>
             <tr>
               <td>
-                <input type="button" value="4" />
+                <input type="button" onClick={this.handleClick} value="4" />
               </td>
               <td>
-                <input type="button" value="5" />
+                <input type="button" onClick={this.handleClick} value="5" />
               </td>
               <td>
-                <input type="button" value="6" />
+                <input type="button" onClick={this.handleClick} value="6" />
               </td>
               <td className="last-column">
-                <input type="button" value="-" />
+                <input type="button" onClick={this.handleClick} value="-" />
               </td>
             </tr>
             <tr>
               <td>
-                <input type="button" value="1" />
+                <input type="button" onClick={this.handleClick} value="1" />
               </td>
               <td>
-                <input type="button" value="2" />
+                <input type="button" onClick={this.handleClick} value="2" />
               </td>
               <td>
-                <input type="button" value="3" />
+                <input type="button" onClick={this.handleClick} value="3" />
               </td>
               <td className="last-column">
-                <input type="button" value="+" />
+                <input type="button" onClick={this.handleClick} value="+" />
               </td>
             </tr>
             <tr className="last-row">
               <td>
-                <input type="button" value="0" />
+                <input type="button" onClick={this.handleClick} value="0" />
               </td>
               <td>
-                <input type="button" value="." />
+                <input type="button" onClick={this.handleClick} value="." />
               </td>
               <td className="last-column">
-                <input type="button" value="=" />
+                <input type="button" onClick={this.handleClick} value="=" />
               </td>
             </tr>
           </tbody>
